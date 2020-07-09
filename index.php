7c2eb9755fdc50acbd8ecd4aebf17d18f270fd64
contato@grudigital.com.br <2423wxci290989>
2020-07-09 18:43:25 -0300

    @felipe-sergio - correcoes de arquivo base

--- index.php
+++ index.php
@@ -1,90 +1,79 @@
 <!doctype html>
 <html lang="pt-br">
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <style type="text/css">
-    .carousel-size {
-      max-height: 900px;
-    }
-  </style>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-  <title>plug with us</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style type="text/css">
+    </style>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Plug with us</title>
 </head>
-<body>  
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<body>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="img/banner1.png" class="d-block w-100" alt="img/banner.png">
-        <div class="carousel-caption">
-          <h3>Puxamos pelo POWER da sua marca.</h3>
-          <p>PWU</p>
-          <button type="button" class="btn btn-outline-info">Saiba mais</button>
+        <div class="carousel-item active">
+            <img src="img/banner1.png" class="d-block w-100" alt="img/banner.png">
+            <div class="carousel-caption">
+                <h3>Puxamos pelo POWER da sua marca.</h3>
+                <p>PWU</p>
+                <button type="button" class="btn btn-outline-info">Saiba mais</button>
+            </div>
         </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img/banner1.png" class="d-block w-100" alt="img/banner.png">
-        <div class="carousel-caption">
-          <h3>Puxamos pelo POWER da sua marca.</h3>
-          <p>PWU</p>
-          <button type="button" class="btn btn-outline-info">Saiba mais</button>
+        <div class="carousel-item">
+            <img src="img/banner1.png" class="d-block w-100" alt="img/banner.png">
+            <div class="carousel-caption">
+                <h3>Puxamos pelo POWER da sua marca.</h3>
+                <p>PWU</p>
+                <button type="button" class="btn btn-outline-info">Saiba mais</button>
+            </div>
         </div>
-      </div>
-      <div class="carousel-item">
-        <img src="img/banner1.png" class="d-block w-100" alt="img/banner.png">
-        <div class="carousel-caption">
-          <h3>Puxamos pelo POWER da sua marca.</h3>
-          <p>PWU</p>
-          <button type="button" class="btn btn-outline-info">Saiba mais</button>
+        <div class="carousel-item">
+            <img src="img/banner1.png" class="d-block w-100" alt="img/banner.png">
+            <div class="carousel-caption">
+                <h3>Puxamos pelo POWER da sua marca.</h3>
+                <p>PWU</p>
+                <button type="button" class="btn btn-outline-info">Saiba mais</button>
+            </div>
         </div>
-      </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
     </a>
     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
     </a>
-  </div>
+</div>
 
-  <nav class="navbar navbar-expand-sm fixed-top navbar-dark">
+<nav class="navbar navbar-expand-sm fixed-top navbar-dark">
     <a class="navbar-brand ml-5" href="#">
-      <img src="img/logo.png" height="30px" alt="">
+        <img src="img/logo.png" height="30px" alt="">
     </a>
-
     <div class="collapse navbar-collapse justify-content-end mr-5" id="navbarSupportedContent">
-      <ul class="navbar-nav text-right  navbar-right link-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">quem somos</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#">o que fazemos</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#">nosso método</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#">contacto</a>
-        </li>
-      </ul>
+        <ul class="navbar-nav text-right  navbar-right link-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">quem somos</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">o que fazemos</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">nosso método</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">contacto</a>
+            </li>
+        </ul>
     </div>
-  </nav>
-
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</nav>
+<script src="js/jquery-3.5.1.slim.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
 </html>
