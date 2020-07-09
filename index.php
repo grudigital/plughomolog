21f8dd01a704eae99c94c32e386cf8c8cf395fd1
contato@grudigital.com.br <2423wxci290989>
2020-07-09 20:31:19 -0300

    @felipe-sergio - contato e rodape ok

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <style type="text/css">
-    </style>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Plug with us</title>
 </head>
 <body>
@@ -72,8 +71,98 @@
         </ul>
     </div>
 </nav>
+
+
+
+
+<section class="bloco-contato-home">
+    <div class="container">
+    <div class="row">
+        <div class="col-8">
+            <span class="bloco-contato-home-contato">Contacto</span><span class="bloco-contato-home-contato-ponto">.</span>
+            <div class="row">
+                <div class="col-6 col-sm-4 espaco30">
+                    <label for="exampleFormControlInput1" class="bloco-contato-home-label">Nome</label>
+                    <input type="text" class="form-control bloco-contato-home-input" id="exampleFormControlInput1" placeholder="Seu nome">
+                </div>
+                <div class="col-6 col-sm-4 espaco30">
+                    <label for="exampleFormControlInput1" class="bloco-contato-home-label">Telefone</label>
+                    <input type="text" class="form-control bloco-contato-home-input" id="exampleFormControlInput1" placeholder="Seu telefone">
+                </div>
+
+                <div class="w-100 d-none d-md-block"></div>
+                <div class="col-6 col-sm-4 espaco30">
+                    <label for="exampleFormControlInput1" class="bloco-contato-home-label">E-mail</label>
+                    <input type="mail" class="form-control bloco-contato-home-input" id="exampleFormControlInput1" placeholder="Seu e-mail"></div>
+                <div class="col-6 col-sm-4 espaco30">
+                    <label for="exampleFormControlInput1" class="bloco-contato-home-label">Assunto</label>
+                    <input type="text" class="form-control bloco-contato-home-input" id="exampleFormControlInput1" placeholder="Digite o assunto">
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="bloco-contato-home-informacoes-texto">Entre em contacto conosco para que possamos energizar sua empresa. <span class="bloco-contato-home-azul">Plug With Us!<span></div>
+            <div class="bloco-contato-home-informacoes-mensagem">Mensagem<span></div>
+            <div class="bloco-contato-home-informacoes-textarea"><textarea class="form-control fundo-escuro-textarea" rows="6" name="mensagem" value="Digite sua mensagem" placeholder="Digite sua mensagem"></textarea></div>
+            <div class="input-group-append rodape-informacoes-botao-enviar"><button class="btn btn-outline-primary rounded-pill rodape-informacoes-enviar" type="button">Enviar</button></div>
+        </div>
+    </div>
+    </div>
+</section>
+
+
+<div class="container rodape-informacoes">
+<div class="row">
+    <div class="col"><img src="img/logorodape.png"></div>
+    <div class="col">
+        <p class="rodape-informacoes-bloco-titulo">Guimarães</p>
+        <p class="rodape-informacoes-bloco-texto">Rua Alto da Bandeira, Nº 847</p>
+    </div>
+    <div class="col">
+        <p class="rodape-informacoes-bloco-titulo">Entre em contato</p>
+        <p class="rodape-informacoes-bloco-texto">agency@plugwithus.com</p>
+    </div>
+    <div class="col">
+        <p class="rodape-informacoes-bloco-titulo">Conecte-se</p>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control rounded rodape-informacoes-campo-email" placeholder="Seu e-mail" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline-primary rounded-pill rodape-informacoes-botao" type="button">Plug With Us</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row rodape-informacoes-linha2">
+    <div class="col"></div>
+    <div class="col">
+        <p class="rodape-informacoes-bloco-titulo">Vila do Conde</p>
+        <p class="rodape-informacoes-bloco-texto">Rua da Lage de Baixo, Nº 216</p>
+    </div>
+    <div class="col">
+        <p class="rodape-informacoes-bloco-titulo">Telefone</p>
+        <p class="rodape-informacoes-bloco-texto">+351 914 997 135</p>
+    </div>
+    <div class="col"></div>
+</div>
+</div>
+
+
+<div class="container">
+    <div class="row justify-content-md-center rodape-copy">
+        <div class="col col-lg-2"></div>
+        <div class="col-md-auto">
+            © 2020, <span class="rodape-copy-plug">PLUG WITH US</span> - <span class="rodape-copy-your">Your Digital Agency</span>. Todos os direitos reservados.
+        </div>
+        <div class="col col-lg-2"></div>
+    </div>
+</div>
+
+
+
+
 <script src="js/jquery-3.5.1.slim.js"></script>
 <script src="js/popper.js"></script>
+<script src="js/style.js"></script>
 <script src="js/bootstrap.js"></script>
 </body>
 </html>
